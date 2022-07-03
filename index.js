4f92127418f62c33725e7c9acffa7607701a215b
Nikita Chistiakov <nikita_chnv@mail.ru>
2022-07-03 16:03:53 +0300

    1

---
+++ index.js
@@ -0,0 +1,9 @@
+var name=prompt ('Введите ваше имя')
+var surname=prompt ('Введите вашу фамилию')
+var patronymic=prompt ('Введите ваше отчество')
+
+var age=prompt ('Сколько вам лет?')
+var gender=confirm ('Ваш пол-мужской?')
+var NSP=('Ваше ФИО:')
+
+alert
